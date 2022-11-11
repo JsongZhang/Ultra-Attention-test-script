9c75299fe2317492d6c73de6ae5d4cec878d987d
unknown <404518267@qq.com>
2022-11-11 13:53:28 +0800

    First Commit

---
+++ Ultra-Attention/utils.py
@@ -0,0 +1,346 @@
+import os
+import sys
+import json
+import pickle
+import random
+
+import torch
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+import os
+import json
+import torch.nn.functional as F
+
+import torch
+from torchvision import transforms, datasets
+import numpy as np
+from tqdm import tqdm
+
+from prettytable import PrettyTable
+
+
+#焦点损失
+
+class FocalLoss(torch.nn.Module):
+    """
+    This is a implementation of Focal Loss with smooth label cross entropy supported which is oposed in
+    'Focal Loss for Dense Object Detection. (https://arxiv.org/abs/1708.02002)'
+        Focal_Loss= -1*alpha*(1-pt)^gamma*log(pt)
+    :param num_class:
+    :param alpha: (tensor) 3D or 4D the scalar factor for this criterion
+    :param gamma: (float,double) gamma > 0 reduces the relative loss for well-classified examples (p>0.5) putting more
+                    focus on hard misclassified example
+    :param smooth: (float,double) smooth value when cross entropy
+    :param balance_index: (int) balance class index, should be specific when alpha is float
+    :param size_average: (bool, optional) By default, the losses are averaged over each loss element in the batch.
+    """
+
+    def __init__(self, num_class, alpha=None, gamma=2, balance_index=-1, smooth=None, size_average=True):
+        super(FocalLoss, self).__init__()
+        self.num_class = num_class
+        self.alpha = alpha
+        self.gamma = gamma
+        self.smooth = smooth
+        self.size_average = size_average
+
+        if self.alpha is None:
+            self.alpha = torch.ones(self.num_class, 1)
+        elif isinstance(self.alpha, (list, np.ndarray)):
+            assert len(self.alpha) == self.num_class
+            self.alpha = torch.FloatTensor(alpha).view(self.num_class, 1)
+            self.alpha = self.alpha / self.alpha.sum()
+        elif isinstance(self.alpha, float):
+            alpha = torch.ones(self.num_class, 1)
+            alpha = alpha * (1 - self.alpha)
+            alpha[balance_index] = self.alpha
+            self.alpha = alpha
+        else:
+            raise TypeError('Not support alpha type')
+
+        if self.smooth is not None:
+            if self.smooth < 0 or self.smooth > 1.0:
+                raise ValueError('smooth value should be in [0,1]')
+
+    def forward(self, input, target):
+        logit = F.softmax(input, dim=1)
+
+        if logit.dim() > 2:
+            # N,C,d1,d2 -> N,C,m (m=d1*d2*...)
+            logit = logit.view(logit.size(0), logit.size(1), -1)
+            logit = logit.permute(0, 2, 1).contiguous()
+            logit = logit.view(-1, logit.size(-1))
+        target = target.view(-1, 1)
+
+
+        epsilon = 1e-10
+        alpha = self.alpha
+        if alpha.device != input.device:
+            alpha = alpha.to(input.device)
+
+        idx = target.cpu().long()
+        one_hot_key = torch.FloatTensor(target.size(0), self.num_class).zero_()
+        one_hot_key = one_hot_key.scatter_(1, idx, 1)
+        if one_hot_key.device != logit.device:
+            one_hot_key = one_hot_key.to(logit.device)
+
+        if self.smooth:
+            one_hot_key = torch.clamp(
+                one_hot_key, self.smooth, 1.0 - self.smooth)
+        pt = (one_hot_key * logit).sum(1) + epsilon
+        logpt = pt.log()
+
+        gamma = self.gamma
+
+        alpha = alpha[idx]
+        loss = -1 * alpha * torch.pow((1 - pt), gamma) * logpt
+
+        if self.size_average:
+            loss = loss.mean()
+        else:
+            loss = loss.sum()
+        return loss
+
+
+
+
+class ConfusionMatrix(object):
+    """
+    This module is used to draw confusion matrix
+
+    """
+    def __init__(self, num_classes: int, labels: list):
+        self.matrix = np.zeros((num_classes, num_classes))
+        self.num_classes = num_classes
+        self.labels = labels
+
+    def update(self, preds, labels):
+        for p, t in zip(preds, labels):
+            self.matrix[p, t] += 1
+
+    def summary(self):
+        # calculate accuracy
+        sum_TP = 0
+        for i in range(self.num_classes):
+            sum_TP += self.matrix[i, i]
+        acc = sum_TP / np.sum(self.matrix)
+        print("the model accuracy is ", acc)
+
+        # precision, recall, specificity
+        table = PrettyTable()
+        table.field_names = ["", "Precision", "Recall", "Specificity"]
+        for i in range(self.num_classes):
+            TP = self.matrix[i, i]
+            FP = np.sum(self.matrix[i, :]) - TP
+            FN = np.sum(self.matrix[:, i]) - TP
+            TN = np.sum(self.matrix) - TP - FP - FN
+            Precision = round(TP / (TP + FP), 3) if TP + FP != 0 else 0.
+            Recall = round(TP / (TP + FN), 3) if TP + FN != 0 else 0.
+            Specificity = round(TN / (TN + FP), 3) if TN + FP != 0 else 0.
+            table.add_row([self.labels[i], Precision, Recall, Specificity])
+        print(table)
+
+    def plot(self):
+        matrix = self.matrix
+        print(matrix)
+        plt.imshow(matrix, cmap=plt.cm.Blues)
+
+        # Set x-axis coordinate label
+        plt.xticks(range(self.num_classes), self.labels, rotation=45)
+        # Set y-axis coordinate label
+        plt.yticks(range(self.num_classes), self.labels)
+        # 显示colorbar
+        plt.colorbar()
+        plt.xlabel('True Labels')
+        plt.ylabel('Predicted Labels')
+        plt.title('Confusion matrix')
+
+        # Mark quantity/probability information in the figure
+        thresh = matrix.max() / 2
+        for x in range(self.num_classes):
+            for y in range(self.num_classes):
+                info = int(matrix[y, x])
+                plt.text(x, y, info,
+                         verticalalignment='center',
+                         horizontalalignment='center',
+                         color="white" if info > thresh else "black")
+        plt.tight_layout()
+        plt.show()
+
+def read_split_data(root: str, val_rate: float = 0.2):
+    random.seed(0)
+    assert os.path.exists(root), "dataset root: {} does not exist.".format(root)
+
+    # Iterate through folders, one folder corresponds to one category
+    Liver_class = [cla for cla in os.listdir(root) if os.path.isdir(os.path.join(root, cla))]
+    # Sort to ensure consistent order
+    Liver_class.sort()
+    # Generate category names and corresponding numerical indexes
+    class_indices = dict((k, v) for v, k in enumerate(Liver_class))
+    json_str = json.dumps(dict((val, key) for key, val in class_indices.items()), indent=4)
+    with open('class_indices_12c.json', 'w') as json_file:
+        json_file.write(json_str)
+
+    train_images_path = []
+    train_images_label = []
+    val_images_path = []
+    val_images_label = []
+    every_class_num = []
+    supported = [".jpg", ".JPG", ".png", ".PNG", "bmp"]
+
+    for cla in Liver_class:
+        cla_path = os.path.join(root, cla)
+
+        images = [os.path.join(root, cla, i) for i in os.listdir(cla_path)
+                  if os.path.splitext(i)[-1] in supported]
+
+        image_class = class_indices[cla]
+
+        every_class_num.append(len(images))
+
+        val_path = random.sample(images, k=int(len(images) * val_rate))
+
+        for img_path in images:
+            if img_path in val_path:
+                val_images_path.append(img_path)
+                val_images_label.append(image_class)
+            else:
+                train_images_path.append(img_path)
+                train_images_label.append(image_class)
+
+    print("{} images were found in the dataset.".format(sum(every_class_num)))
+    print("{} images for training.".format(len(train_images_path)))
+    print("{} images for validation.".format(len(val_images_path)))
+
+    plot_image = False
+    if plot_image:
+        # Draw the number histogram of each category
+        plt.bar(range(len(Liver_class)), every_class_num, align='center')
+        plt.xticks(range(len(Liver_class)), Liver_class)
+        for i, v in enumerate(every_class_num):
+            plt.text(x=i, y=v + 5, s=str(v), ha='center')
+        plt.xlabel('image class')
+        plt.ylabel('number of images')
+        plt.title('flower class distribution')
+        plt.show()
+
+    return train_images_path, train_images_label, val_images_path, val_images_label
+
+
+def plot_data_loader_image(data_loader):
+    batch_size = data_loader.batch_size
+    plot_num = min(batch_size, 4)
+
+    json_path = 'Liver_13.json'
+    assert os.path.exists(json_path), json_path + " does not exist."
+    json_file = open(json_path, 'r')
+    class_indices = json.load(json_file)
+
+    for data in data_loader:
+        images, labels = data
+        for i in range(plot_num):
+            # [C, H, W] -> [H, W, C]
+            img = images[i].numpy().transpose(1, 2, 0)
+            # Anti Normalize Operation
+            img = (img * [0.229, 0.224, 0.225] + [0.485, 0.456, 0.406]) * 255
+            label = labels[i].item()
+            plt.subplot(1, plot_num, i+1)
+            plt.xlabel(class_indices[str(label)])
+            plt.xticks([])
+            plt.yticks([])
+            plt.imshow(img.astype('uint8'))
+        plt.show()
+
+
+def write_pickle(list_info: list, file_name: str):
+    with open(file_name, 'wb') as f:
+        pickle.dump(list_info, f)
+
+
+def read_pickle(file_name: str) -> list:
+    with open(file_name, 'rb') as f:
+        info_list = pickle.load(f)
+        return info_list
+
+
+def train_one_epoch(model, optimizer, data_loader, device, epoch):
+    model.train()
+    #loss_function = torch.nn.CrossEntropyLoss()
+    loss_function = FocalLoss(num_class=13)
+    accu_loss = torch.zeros(1).to(device)
+    accu_num = torch.zeros(1).to(device)
+    optimizer.zero_grad()
+
+    sample_num = 0
+    data_loader = tqdm(data_loader, file=sys.stdout)
+    for step, data in enumerate(data_loader):
+        images, labels = data
+        sample_num += images.shape[0]
+
+        pred = model(images.to(device))
+        pred_classes = torch.max(pred, dim=1)[1]
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+
+        loss = loss_function(pred, labels.to(device))
+        loss.backward()
+        accu_loss += loss.detach()
+
+        data_loader.desc = "[train epoch {}] loss: {:.3f}, acc: {:.3f}".format(epoch,
+                                                                               accu_loss.item() / (step + 1),
+                                                                               accu_num.item() / sample_num)
+
+        if not torch.isfinite(loss):
+            print('WARNING: non-finite loss, ending training ', loss)
+            sys.exit(1)
+
+        optimizer.step()
+        optimizer.zero_grad()
+
+    return accu_loss.item() / (step + 1), accu_num.item() / sample_num
+
+
+@torch.no_grad()
+def evaluate(model, data_loader, device, epoch):
+    loss_function = torch.nn.CrossEntropyLoss()
+
+    model.eval()
+
+    accu_num = torch.zeros(1).to(device)
+    accu_loss = torch.zeros(1).to(device)
+
+    sample_num = 0
+    data_loader = tqdm(data_loader, file=sys.stdout)
+    for step, data in enumerate(data_loader):
+        images, labels = data
+        sample_num += images.shape[0]
+
+        pred = model(images.to(device))
+        pred_classes = torch.max(pred, dim=1)[1]
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+
+        loss = loss_function(pred, labels.to(device))
+        accu_loss += loss
+
+        data_loader.desc = "[valid epoch {}] loss: {:.3f}, acc: {:.3f}".format(epoch,
+                                                                               accu_loss.item() / (step + 1),
+                                                                               accu_num.item() / sample_num)
+
+    json_label_path = './Liver_13.json'
+    assert os.path.exists(json_label_path), "cannot find {} file".format(json_label_path)
+    json_file = open(json_label_path, 'r')
+    class_indict = json.load(json_file)
+
+    labels = [label for _, label in class_indict.items()]
+    confusion = ConfusionMatrix(num_classes=13, labels=labels)
+    # net.eval()
+    if epoch == 299:
+        with torch.no_grad():
+            for val_data in tqdm(data_loader):
+                val_images, val_labels = val_data
+                outputs = model(val_images.to(device))
+                outputs = torch.softmax(outputs, dim=1)
+                outputs = torch.argmax(outputs, dim=1)
+                confusion.update(outputs.to("cpu").numpy(), val_labels.to("cpu").numpy())
+        confusion.plot()
+        confusion.summary()
+    return accu_loss.item() / (step + 1), accu_num.item() / sample_num
